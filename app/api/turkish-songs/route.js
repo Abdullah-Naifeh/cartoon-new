bd0f7333ce4a31369b19b622d52775d4b69a25fc
Abdullah <104142757+Abdullah-Naifeh@users.noreply.github.com>
2025-09-07 12:49:05 +0300

    Update route.js

--- app/api/turkish-songs/route.js
+++ app/api/turkish-songs/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '../../../lib/prisma';
+import  prisma  from '../../../lib/prisma';
 
 // GET - جلب جميع الأغاني التركية
 export async function GET(request) {
